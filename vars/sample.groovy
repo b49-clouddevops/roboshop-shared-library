a978cf3023c6513d0bd21027983b7054ba0992d3
mvpc-11232 <mvp2612@hotmail.com>
2022-08-06 08:36:48 +0530

    Auto Push Commit

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,5 +1,5 @@
 def info(message, URL) {    // Declaring a function 
-    echo "INFO: ${message}  ,url value is ${URL}"
+    echo "INFO: ${message} ,url value is ${URL}"
 }
 
 info("Hai" , "twitter.com")           // calling a function
