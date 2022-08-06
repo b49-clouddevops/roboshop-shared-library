23ed350ac6b6754928145a3d36881267d2a86b1f
mvpc-11232 <mvp2612@hotmail.com>
2022-08-06 08:44:49 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,3 @@
-def lintcheck(message, URL) {    // Declaring a function 
-    echo "INFO: ${message} ,url value is ${URL}"
-}
-
-info("Hai" , "twitter.com")           // calling a function
-
-
 def call() {
 pipeline {
     agent any 
