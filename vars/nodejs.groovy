cafc8f130dc11289c95156a3d7e8d1b35c32927a
mvpc-11232 <mvp2612@hotmail.com>
2022-08-06 08:53:08 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,22 +6,15 @@ pipeline {
         stage('Lint checks') {
             steps {
                 script {
-                      lintcheck(COMPONENT)
+                        echo "lint checks started for ${COMPONENT}"
+                        sh "echo installing jslint"
+                        sh "npm install jslint"
+                        sh "node_modules/jslint/bin/jslint.js server.js || true"
+                        sh "echo lint checks completed"
                     }
                 }
             }
         } // end of the stages
     }  // end of the pipeline
 }  // end of function call 
-
-
-// defining the lint check function
-def lintcheck(COMPONENT) {    
-    echo "lint checks started for ${COMPONENT}"
-     sh "echo installing jslint"
-     sh "npm install jslint"
-     sh "node_modules/jslint/bin/jslint.js server.js || true"
-     sh "echo lint checks completed"
-
-}
 
