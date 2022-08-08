55653be285102d16b4bf26dd078704dd4841ff3e
mvpc-11232 <mvp2612@hotmail.com>
2022-08-08 08:00:05 +0530

    Auto Push Commit

---
+++ vars/payment.groovy
@@ -0,0 +1,23 @@
+def lintChecks() {
+  sh '''
+    echo lint checks starting for ${COMPONENT}
+    mvn checkstyle:check
+    echo lint checks completed for ${COMPONENT}
+    '''
+}
+
+def call() {     // call is the default which will be called
+pipeline {
+    agent any 
+    stages {
+        // This should run for every commit on feature branch
+        stage('Lint checks') {
+            steps {
+                script {
+                     lintChecks()
+                    }
+                }
+            }
+        } // end of the stages
+    }  // end of the pipeline
+}  // end of function call 
