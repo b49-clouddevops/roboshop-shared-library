b80bd9d45012bf8b88223b1ca80257a87a4ef38a
mvpc-11232 <mvp2612@hotmail.com>
2022-08-09 08:07:12 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -12,6 +12,7 @@ def sonarCheck() {
     mvn clean compile
     pwd && ls -ltr
     sonar-scanner -Dsonar.host.url=http://172.31.4.93:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    
    '''
 }
 
