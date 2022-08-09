8c17c6630552d6849e1fd8ae22c4bc7d84690aad
mvpc-11232 <mvp2612@hotmail.com>
2022-08-09 08:18:13 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -8,9 +8,7 @@ def lintChecks() {
 
 def sonarCheck() {
   sh '''
-    mvn --version
     mvn clean compile
-    pwd && ls -ltr
     sonar-scanner -Dsonar.host.url=http://172.31.4.93:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
     curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
     chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.93 ${COMPONENT}
