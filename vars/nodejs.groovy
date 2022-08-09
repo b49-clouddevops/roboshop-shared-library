50f2b7129f16190d34330198baa39d3a38671af7
mvpc-11232 <mvp2612@hotmail.com>
2022-08-09 08:18:52 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,6 +7,14 @@ def lintChecks() {
     '''
 }
 
+def sonarCheck() {
+  sh '''
+    sonar-scanner -Dsonar.host.url=http://172.31.4.93:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
+    chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.93 ${COMPONENT}
+   '''
+}
+
 def call() {     // call is the default which will be called
 pipeline {
     agent any 
