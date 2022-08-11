aee9facec2603d8e2e2010fef0efaa2a64b747f0
mvpc-11232 <mvp2612@hotmail.com>
2022-08-11 07:48:11 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -17,7 +17,7 @@ def sonarCheck() {
    '''
 }
 
-def sonarCheck() {
+def testCases() {
   sh '''
     // sonar-scanner -Dsonar.host.url=http://172.31.4.93:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}
     // curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
