8dfe8a86bce90a167d6ba845e4c997bb3774d3af
mvpc-11232 <mvp2612@hotmail.com>
2022-08-12 08:14:40 +0530

    Auto Push Commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -82,6 +82,7 @@ pipeline {
         stage('Uploading Artifacts') {
             when { 
                expression { env.TAG_NAME != null }
+               expression { env.UPLOAD_STATUS == "" }
             }   
             steps {
                sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.8.134:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
