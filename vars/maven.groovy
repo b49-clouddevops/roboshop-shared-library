874aebb319d9b7d4b956685499647ea78e674c00
mvpc-11232 <mvp2612@hotmail.com>
2022-08-13 08:30:50 +0530

    Auto Push Commit

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,8 +1,14 @@
 env.APP_TYPE="java"
 def call() {
     node {
+        git branch: 'main', url: 'https://github.com/b49-clouddevops/catalogue'
+        env.APP_TYP = "java"
         common.lintChecks()
         common.sonarCheck()
+        common.testCases()
+        if(env.TAG_NAME != null) {
+        common.artifacts()
+        }
     }
 }
 
