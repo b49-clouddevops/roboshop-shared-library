27835691a2977ac8d46731c35688fe004190df15
mvpc-11232 <mvp2612@hotmail.com>
2022-08-13 09:10:39 +0530

    Auto Push Commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -95,7 +95,7 @@ def artifacts() {
                 sh "go mod init ${COMPONENT}"
                 sh "go get"
                 sh "go build"
-                sh "zip -r ${COMPONENT}-${TAG_NAME}.zip "
+                sh "zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}"
            }          
         }
      
