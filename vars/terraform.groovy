bbe9347848fdc09339d219ec2e5d972d963ebcfe
mvpc-11232 <mvp2612@hotmail.com>
2022-08-27 07:47:55 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -11,5 +11,14 @@ def call {
             sh "terrafile -f env-${ENV}/Terrafile"
             sh "sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars""
         }
+
+        stage('terraform plan') {
+                sh "echo doing a terrafomr plan"
+                sh "terraform plan -var-file=env-${ENV}/${ENV}.tfvars"
+        }
+
+        stage('terraform apply') {
+                sh "terraform apply -var-file=env-${ENV}/${ENV}.tfvars -auto-approve"
+        }
     }
 }
