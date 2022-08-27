78fda94aa7a221724267c1a5786398bc6e01f15c
mvpc-11232 <mvp2612@hotmail.com>
2022-08-27 07:45:24 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,8 +1,15 @@
+def call {
 
+    properties([
+        parameters([
+            choice(choices: ['fix\nmajor\nminor\nnone'], description: "Increase version's number: MAJOR.MINOR.FIX", name: "VERSIONING"),
+        ]),
+    ])
 
-node {
-    stage('terraform init'){
-         sh "terrafile -f env-${ENV}/Terrafile"
-         sh "sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars""
+    node {
+        stage('terraform init'){
+            sh "terrafile -f env-${ENV}/Terrafile"
+            sh "sh "terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars""
+        }
     }
 }
