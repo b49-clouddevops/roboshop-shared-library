b8b85e26f85c8f3f46037159ddcff8ac77fd75f8
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 21:17:52 +0530

    Auto Push Commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -42,5 +42,5 @@ def call() {
 }
 
 
-// terraform-mutable will be there only for components and not for the DB's. Since we use the same code here, we need to ensure for DB's it' don't have to cd to terraform_dir
-// and executre tf commands. But for APP Components, it's to go to terraform-mutable and execute the terraform commands
+terraform-mutable will be there only for components and not for the DB's. Since we use the same code here, we need to ensure for DB's it' don't have to cd to terraform_dir
+and executre tf commands. But for APP Components, it's to go to terraform-mutable and execute the terraform commands
