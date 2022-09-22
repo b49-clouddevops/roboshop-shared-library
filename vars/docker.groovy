a573004aa38ecba5643b25a38c38769d0caa5c91
mvpc-11232 <mvp2612@hotmail.com>
2022-09-22 09:01:04 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -3,6 +3,7 @@ def call() {
         sh "rm -rf *"
         git branch: 'main', url: "https://github.com/b49-clouddevops/${COMPONENT}"
         stage('Docker Build') {
+            
             sh "docker  build ."
         }
 
