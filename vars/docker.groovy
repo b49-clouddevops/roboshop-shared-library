cc9a2a536cc3ef6ad28e82e77f41f225ac724cb6
mvpc-11232 <mvp2612@hotmail.com>
2022-09-23 07:55:28 +0530

    Auto Push Commit

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -9,7 +9,7 @@ def call() {
         if(env.TAG_NAME != null) {
         stage('Docker Push') {
             sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}" 
-            sh "docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest"
+            sh "docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
            }
         }
     }
